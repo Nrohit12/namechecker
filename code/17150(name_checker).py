fdc41b1193d3d8d0cbcbf46d4b7c0928bfec545c
Rohit Raj <33140252+Nrohit12@users.noreply.github.com>
2020-03-03 13:21:37 +0530

    Add files via upload

---
+++ code/17150(name_checker).py
@@ -0,0 +1,41 @@
+# Taking input from user
+
+name = input("Enter the hindi name: \n")
+
+# Checking for the length of String
+
+length = len(name)
+
+######if last letter is a vowel, it is a Woman else it might be a Man and move to the next step
+  
+
+if name[length-1] in ('a', 'e', 'i', 'o', 'u'):
+    print(name,": is the name of a woman")
+else :
+    print(name,": may be the name of a man, check with the next step :")
+    # Moving on to next classifier, sonorants
+    nameSONO = name[::-1]
+    ##### getting last 3 chars of the name as a list ######  
+    array1 = []
+    for i in range(0,3):
+        array1.append(nameSONO[i])
+    ##### reversing the sonorant string and converting the list into a string ######
+    sono = ''.join(array1)
+    sonorant = sono[::-1]
+    # print("The sonorant is ", sonorant)
+    ### comparing the sonorant with the strings of our corpus ###
+    if sono in ("mha", "nha", "lha", "vha", "rha"):
+        print(name,": is the name of a woman")
+    else: 
+        print(name,": still not sure, may be a man, check with the next step:")
+        ### checking open syllables from our corpus ###
+        if sonorant in ("rin","be", "go", "hi","aja","avi", "kha", "eha","ali","eet"):
+            print(name,":  is the name of a woman")
+        else:
+            print(name,": The best probability is,he is a man ")
+
+# The above list can be increased depending upon the corpus of hindi words
+
+## link for project https://github.com/Nrohit12/namechecker ##
+
+
